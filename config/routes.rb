3f6060439485976abfab2aaf5dad157a8fc052dc
Kang-Kyu Lee <kangkyu1111@gmail.com>
2019-04-28 22:10:10 -0700

    Add link to each book route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :files, only: [:create, :new]
-  get '/:id' => "verses#index"
+  get '/:id' => "verses#index", as: 'book'
   root "verses#index"
 end
